7b02460f63443d6a0914696fe559ed2a8a6a566d
ben.sturgis <ben@sturgis@anavs.de>
2024-08-07 12:00:13 +0200

    chore(config): add configuration files to train PV-RCNN model on custom recombination dataset

--- pcdet/datasets/custom/custom_dataset.py
+++ pcdet/datasets/custom/custom_dataset.py
@@ -277,7 +277,7 @@ if __name__ == '__main__':
         ROOT_DIR = (Path(__file__).resolve().parent / '../../../').resolve()
         create_custom_infos(
             dataset_cfg=dataset_cfg,
-            class_names=['Vehicle', 'Pedestrian', 'Cyclist'],
+            class_names = dataset_cfg.CLASS_NAMES,
             data_path=ROOT_DIR / 'data' / 'custom',
             save_path=ROOT_DIR / 'data' / 'custom',
         )
